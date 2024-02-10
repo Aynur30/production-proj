d0491241989679578fd7cf5dd9391d759bfae117
Aynur30 <aynur.shakirov.00.00@mail.ru>
2024-02-10 22:06:55 +0500

    added auth modal

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,12 @@
-import React, {Suspense} from 'react';
-import './styles/index.scss';
-import {classNames} from "shared/lib/classNames/classNames";
-import {useTheme} from "app/providers/ThemeProvider";
-import {AppRouter} from "app/providers/router";
-import {Navbar} from "widgets/Navbar";
-import {Sidebar} from "widgets/Sidebar";
+import React, { Suspense } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
 
-
-const App = () => {
-   const { theme } = useTheme();
+function App() {
+    const { theme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
@@ -21,6 +19,6 @@ const App = () => {
             </Suspense>
         </div>
     );
-};
+}
 
 export default App;
